8144f4072ab97d0d75bae59affd9207f55492f84
Abhirup Roy (International Supplier) <v-abhiruproy@microsoft.com>
2025-09-09 23:26:40 +0530

    updated code to use newtonsoft.json

--- Cobra.Data/Class1.cs
+++ Cobra.Data/Class1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,19 +9,28 @@ namespace Cobra.Data
 {
     public class Class1
     {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+
         public Class1()
         {
-            myData();
+            // Removed myData() call from constructor to prevent infinite recursion
         }
 
         public string myData()
         {
-            string appName = "Cobra";
-            appName = appName + " Website Deploy Test From Class Library";
-            return appName;
+            string jsonString = @"{
+                'Name': 'John Doe',
+                'Age': 30,
+                'City': 'New York'
+            }";
 
+            // Use a separate class or anonymous object for deserialization
+            var personData = JsonConvert.DeserializeAnonymousType(jsonString, new { Name = "", Age = 0, City = "" });
+            string appName = "Cobra";
+            appName = appName + " Website Deploy Test From Class Library for " + personData.Name + " in " + personData.City + " age: " + personData.Age;
+            return appName;
         }
     }
-
-
 }
